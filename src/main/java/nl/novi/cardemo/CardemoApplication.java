8576e45c3dcbefa0c64a445b5d2e61a21569fa30
FransvanEk <frans@fransvanek.nl>
2025-01-22 10:37:49 +0100

    Initial commit

---
+++ src/main/java/nl/novi/cardemo/CardemoApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.cardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/cardemo/CardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.cardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
